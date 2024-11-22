b13ec8502a77811a7cc87f5d04015e187c5ae23d
amityadav1996 <yadav02amit1996@gmail.com.>
2024-11-22 18:55:59 +0530

    first commit Success

---
+++ tt.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter number1:"))
+num2 = int(input("enter number2:"))
+num3 = int(input("enter number3:"))
+
+if num1>num2 and num3:
+    largest = num1
+elif num2>=num1 and num2>=num3:
+    largest = num2
+else:
+    num3>=num1 and num3>=num2
+    largest = num3
+
+    print("the largest num is:", largest)
+
+
+
